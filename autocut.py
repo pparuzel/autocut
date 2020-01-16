ce72bab9452135b95e79a1c9b8eb8e3b0ccb78ba
Pawel Paruzel <pawelparuzel95@gmail.com>
2020-01-16 02:26:28 +0100

    Refactor and tweak

--- autocut.py
+++ autocut.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python3
 
 import os
-import sys
 import math
 import argparse
 import subprocess as sub
 
+from sys import stderr, exit
 from datetime import timedelta
 from tempfile import mkdtemp
 
@@ -27,15 +27,10 @@ class AutoCut:
         output_base_name -- base name of an output file
         config           -- namespace of an argument parser
         """
-        try:
-            sub.check_call(['ffmpeg', '-h'], stdout=sub.DEVNULL, stderr=sub.DEVNULL)
-            sub.check_call(['ffprobe', '-h'], stdout=sub.DEVNULL, stderr=sub.DEVNULL)
-        except FileNotFoundError:
-            print('error: could not find ffmpeg/ffprobe', file=sys.stderr)
-            sys.exit(1)
+        self.check_utilities('ffmpeg', 'ffprobe')
         if not os.path.exists(input_file):
-            print('error: file does not exist', file=sys.stderr)
-            sys.exit(1)
+            print('error: file does not exist', file=stderr)
+            exit(1)
         self.config = config if config else EmptyConfig()
         self.input_file = input_file
         input_base, extension = os.path.splitext(self.input_file)
@@ -47,13 +42,10 @@ class AutoCut:
 
         rms_threshold -- Maximum RootMeansSquare threshold level of noise
         """
-        segments = self.audio_level_segmentation(self.input_file, rms_threshold)
+        segments = self.audio_level_segmentation(rms_threshold)
         if not segments:
-            print(f'warning: no cuts received')
+            print(f'warning: no cuts received', file=stderr)
             return
-        # if math.log(len(segments), 10) > 3:
-        #     print(f'error: too many segments')
-        #     sys.exit(1)
         temp_dir = mkdtemp(
             prefix=f'autocut_{self.output_base}_',
             dir=os.getcwd())
@@ -73,7 +65,7 @@ class AutoCut:
                     print(f'successfully created {file_base}')
                     segments_file.write(f'{file_base}: {segment_pair}\n')
                 else:
-                    print(f'error: could not create {output_file}')
+                    print(f'error: could not create {output_file}', file=stderr)
                     segments_file.write(f'<no file>: {segment_pair}\n')
         if self.config.dry_run and self.config.verbose:
             with open(segments_path, 'r') as segments_file:
@@ -112,7 +104,11 @@ class AutoCut:
         # print(f'process stderr: {err.decode()}')
         return edit_process.returncode == 0
 
-    def audio_level_segmentation(self, filepath, threshold):
+    def audio_level_segmentation(self, threshold):
+        """Performs audio-level analysis and returns segments
+
+        threshold -- Maximum RootMeansSquare threshold level of noise
+        """
         def get_levels_in_time(probe_process):
             for line in probe_process.stdout:
                 yield map(float, line.decode().strip().split(','))
@@ -122,7 +118,7 @@ class AutoCut:
             '-f',
             'lavfi',
             '-i',
-            f'amovie={filepath},astats=metadata=1:reset=1',
+            f'amovie={self.input_file},astats=metadata=1:reset=1',
             '-show_entries',
             'frame=pkt_pts_time:frame_tags=lavfi.astats.Overall.RMS_level',
             '-of',
@@ -130,7 +126,7 @@ class AutoCut:
         ]
         probe_process = sub.Popen(args, stdout=sub.PIPE, stderr=sub.PIPE)
         volumes = {}  # for the purpose of averaging
-        print(f'analyzing audio of {filepath} ...')
+        print(f'analyzing audio of {self.input_file} ...')
         for timestamp, volume, *rest in get_levels_in_time(probe_process):
             average = volumes.setdefault(
                 round(round(timestamp * 10) / 10, 1),
@@ -169,6 +165,25 @@ class AutoCut:
         if segments and segments[0] and segments[0][0] < 0:
             del segments[0]
         return segments
+
+    def check_utilities(self, *utilities):
+        """Checks whether utilities exist
+        and return help message
+
+        *utilities -- string parameters with names of utilities
+        """
+        must_exit = False
+        for utility in utilities:
+            try:
+                sub.check_call(
+                    [utility, '-h'],
+                    stdout=sub.DEVNULL,
+                    stderr=sub.DEVNULL)
+            except FileNotFoundError:
+                print(f'error: could not find {utility}', file=stderr)
+                must_exit = True
+        if must_exit:
+            exit(1)
 
 
 def run_autocut():
@@ -213,6 +228,6 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         pass
     except Exception as e:
-        print('error: caught unexpected exception')
-        print(e)
+        print('error: caught unexpected exception', file=stderr)
+        print(e, file=stderr)
         raise e
